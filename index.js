88b5fe2190837101068bf410b7f8153791bf742c
ItelSunday <iteldomingotech@gmail.com>
2019-04-01 13:34:09 -0700

    port running

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+// const bcryptjs = require('bcryptjs');
+
+// const db = require('./database/dbConfig.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+const port = process.env.PORT || 5500;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
+
+
+
