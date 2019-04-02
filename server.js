d2e2138f43dd3631dd9cc516aeede3169c45a54f
ItelSunday <iteldomingotech@gmail.com>
2019-04-02 06:35:15 -0700

    installed and imported bcrypt to server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const encrypt = require('bcrypt.js');
 
 const server = express();
-// const bcryptjs = require('bcryptjs');
 
 // const db = require('./database/dbConfig.js');
 
