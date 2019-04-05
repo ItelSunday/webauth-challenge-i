3c38fbf23050dfcf230c7f41dcff507dde1ba3ef
ItelSunday <iteldomingotech@gmail.com>
2019-04-04 17:19:34 -0700

    updated hash, .env

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const secret = require('../api/secrets').jwtSecret 
+const secret = require('../api/secrets').jwtSecret;
 const Users = require('../users/users-model');
 
 // POST - REGISTER
-router.post('/register', async (req, res) => {
+router.post('/register', (req, res) => {
     let credentials = req.body
     const hash = bcrypt.hashSync(credentials.password, 12);
         credentials.password = hash;
@@ -21,27 +21,29 @@ router.post('/register', async (req, res) => {
       });
   });
 
-
-  // POST - LOGIN
-router.post('/login', async (req, res) => {
-    let {username, password} = req.body;
-
-    Users.findBy({username})
-    .first()
-    .then(user => {
-        const token = generateToken(user); 
-        if(user && bcrypt.compareSync(password, user.password)) {
-            res.status(200).json({
-              message: `Welcome ${user.username}`,
-              token
-            })
-          } else {
-            res.status(401).json({ message: "Invalid credentials"})
-      }
-    }) .catch (error => {
-        res.status(500).json({message: 'login error'});
+  // POST - LOGIN - Register works with token | gives Error 500 when logging in
+  router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+  
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
       });
-    });
+  });
+    
     
     
     // Generate Token
@@ -57,21 +59,21 @@ router.post('/login', async (req, res) => {
     }
         
   
-    // GET - LOGOUT
-  router.get('/logout', (req, res) => {
+  //   // GET - LOGOUT
+  // router.get('/logout', (req, res) => {
   
-    if(req.session.user) {
-      req.session.destroy(err => {
-        if(err) {
-          res.status(500).json({ message: "Something went wrong"}) //if there is a user but fails to delete the session
-        } else {
-          res.status(200).json({ message: "You are now logged out"}) // if it does find the user..
-        }
-      });
-    } else {
-      res.status(400).json({message: "login required"}) //if it doesn't find the user then this/..
-    }
-  });
+  //   if(req.session.user) {
+  //     req.session.destroy(err => {
+  //       if(err) {
+  //         res.status(500).json({ message: "Something went wrong"}) //if there is a user but fails to delete the session
+  //       } else {
+  //         res.status(200).json({ message: "You are now logged out"}) // if it does find the user..
+  //       }
+  //     });
+  //   } else {
+  //     res.status(400).json({message: "login required"}) //if it doesn't find the user then this/..
+  //   }
+  // });
 
 
 
